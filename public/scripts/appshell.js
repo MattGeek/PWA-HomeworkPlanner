9a6d0d7da119085061177e36455d99637ec8aef8
MattGeek <solecustode109@gmail.com>
2019-05-17 08:43:26 +0200

    Version 0.2

--- public/scripts/appshell.js
+++ public/scripts/appshell.js
@@ -1,14 +1,9 @@
-(function(){
-  
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAO-NtqggPle6tPcTn5LVoKbKHgpy16nEs",
-    authDomain: "homeworkplanner-2a42.firebaseapp.com",
-    projectId: "homeworkplanner-2a42",
-    storageBucket: "homeworkplanner-2a42.appspot.com",
-    messagingSenderId: "404114046838"
-  };
-  firebase.initializeApp(config);
-  const auth = firebase.auth();
+(
+  ()=>{
+    const auth = firebase.auth();
 
-}());
+    firebase.firestore().enablePersistance().then(() => {
+    const firestore = app.firestore();
+    });
+  }
+);
