2a3cad584c6f002095fa31a259be13302785ecdc
MattGeek <solecustode109@gmail.com>
2019-05-17 09:48:15 +0200

    Version 0.2.1

--- public/scripts/appshell.js
+++ public/scripts/appshell.js
@@ -5,18 +5,18 @@
     firebase.firestore().enablePersistance().then(() => {
       const db = app.firestore();
 
-      var weekDatesR = document.getElementById('weekDates');
-      var homework = document.getElementById('homework');
+    //  var weekDatesR = document.getElementById('weekDates');
+      //var homework = document.getElementById('homework');
 
-      db.collection('homework').get()
+      db.collection('subkect').get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          console.log(doc.id, " => ", doc.data());
-        });
+          console.log(doc.id, " => ", doc.data())
+        })
       })
       .catch(error => {
-        console.log("Error getting documents: ", error);
-      });
-    });
+        console.log("Error getting documents: ", error)
+      })
+    })
   }
 );
