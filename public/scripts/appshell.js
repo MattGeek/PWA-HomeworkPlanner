e4be72252863dde72b7d2b835fc700767927b3e3
MattGeek <solecustode109@gmail.com>
2019-05-17 09:24:20 +0200

    Version 0.2.1

--- public/scripts/appshell.js
+++ public/scripts/appshell.js
@@ -3,7 +3,21 @@
     const auth = firebase.auth();
 
     firebase.firestore().enablePersistance().then(() => {
-    const firestore = app.firestore();
+      const db = app.firestore();
+
+      var weekDatesR = document.getElementById('weekDates');
+      var homework = document.getElementById('homework');
+
+      db.collection('homework').where("expiry", "==", Timestamp.fromDate(new Date("2019-05-20T00:00:00+0200")))
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          console.log(doc.id, " => ", doc.data());
+        });
+      });
+      .catch(error => {
+        console.log("Error getting documents: ", error);
+      });
     });
   }
 );
